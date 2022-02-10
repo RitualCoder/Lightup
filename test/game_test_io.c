a6b1501ba7b51745fa27346f12a27952170e70a3
Guillaume Dindart <guillaume.dindart@etu.u-bordeaux.fr>
2022-02-10 18:03:05 +0100

    add test file

---
+++ test/game_test_io.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../libgame/game.h"
+#include "../libgame/game_aux.h"
+#include "game_examples.h"
+#include "../libgame/game_ext.h"
+#include "game_test.h"
+
+
+/* ************************************************************************** */
+/*                               IO TESTS (V2)                                */
+/* ************************************************************************** */
+
+int test_load(void){
+    return EXIT_SUCCESS;
+}
+
+/* ************************************************************************** */
+
+int test_save(){
+    return EXIT_SUCCESS;
+}
