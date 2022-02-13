b1b4359f4f62a1f221ec69ff8d0a56248087108e
Guillaume Dindart <guillaume.dindart@etu.u-bordeaux.fr>
2022-02-13 15:09:10 +0100

    Fix error for missing source

--- libgame/game_tools.c
+++ libgame/game_tools.c
@@ -1,82 +1,82 @@
 #include "game_tools.h"
+
+#include <stdlib.h>
+
 #include "game.h"
 #include "game_ext.h"
 #include "game_private.h"
 
-#include <stdlib.h>
-
-game game_load(char * filename){
-    //Check filename and file existance
-    if(filename == NULL){
+game game_load(char* filename) {
+    // Check filename and file existance
+    if (filename == NULL) {
         fprintf(stderr, "Invalid file name in game_load()\n");
         exit(5);
     }
-    FILE* file= fopen(filename, "r");
+    FILE* file = fopen(filename, "r");
 
-    if(file == NULL){
+    if (file == NULL) {
         fprintf(stderr, "Unable to open file!\n");
         exit(5);
     }
 
-    //Read size and wrapping information
+    // Read size and wrapping information
     uint nb_rows, nb_cols, wrapping;
     int ret;
 
     ret = fscanf(file, " %d  %d %d", &nb_rows, &nb_cols, &wrapping);
 
-    if(nb_rows > 10 || nb_cols > 10 || ret != 3){
+    if (nb_rows > 10 || nb_cols > 10 || ret != 3) {
         fprintf(stderr, "Error when loading game!");
         exit(5);
     }
 
-    //read unit find the first \n for the starting grid
+    // read unit find the first \n for the starting grid
     char c = fgetc(file);
-    while(c != '\n'){
+    while (c != '\n') {
         c = fgetc(file);
     }
-    square* read_data = malloc(sizeof(square)*nb_rows*nb_cols);
-    for(uint i = 0; i < nb_rows*nb_cols; i++){
+    square* read_data = malloc(sizeof(square) * nb_rows * nb_cols);
+    for (uint i = 0; i < nb_rows * nb_cols; i++) {
         c = fgetc(file);
-        switch (c)
-        {
-        case 'b':
-            read_data[i] = S_BLANK;
-            break;
-        case '*':
-            read_data[i] = S_LIGHTBULB;
-            break;
-        case '0':
-            read_data[i] = S_BLACK0;
-            break;
-        case '1':
-            read_data[i] = S_BLACK1;
-            break;
-        case '2':
-            read_data[i] = S_BLACK2;
-            break;
-        case '3':
-            read_data[i] = S_BLACK3;
-            break;
-        case '4':
-            read_data[i] = S_BLACK4;
-            break;
-        case 'w':
-            read_data[i] = S_BLACKU;
-            break;
-        case '-':
-            read_data[i] = S_MARK;
-            break;
-        case '\n':
-            i--;
-            break;
-        default:
-            fprintf(stderr, "unable to read game");
-            //exit(5);
-            break;
+        switch (c) {
+            case 'b':
+                read_data[i] = S_BLANK;
+                break;
+            case '*':
+                read_data[i] = S_LIGHTBULB;
+                break;
+            case '0':
+                read_data[i] = S_BLACK0;
+                break;
+            case '1':
+                read_data[i] = S_BLACK1;
+                break;
+            case '2':
+                read_data[i] = S_BLACK2;
+                break;
+            case '3':
+                read_data[i] = S_BLACK3;
+                break;
+            case '4':
+                read_data[i] = S_BLACK4;
+                break;
+            case 'w':
+                read_data[i] = S_BLACKU;
+                break;
+            case '-':
+                read_data[i] = S_MARK;
+                break;
+            case '\n':
+                i--;
+                break;
+            default:
+                fprintf(stderr, "unable to read game");
+                exit(5);
+                break;
         }
     }
-    
-    //convert int to bool, wrapping
+
+    // convert int to bool, wrapping
     bool bool_wrapping;
     bool_wrapping = wrapping == 0;
 
@@ -88,35 +88,34 @@ game game_load(char * filename){
     return rgame;
 }
 
-void game_save(cgame g, char *filename){
-    FILE* f=fopen(filename ,'w');
-    if (f == NULL){
+void game_save(cgame g, char* filename) {
+    FILE* f = fopen(filename, 'w');
+    if (f == NULL) {
         fprintf(stderr, "inexistant file");
         exit(5);
     }
     uint wall_nb;
     uint wrap;
-    if (g -> wrapping){
+    if (g->wrapping) {
         wrap = 1;
-    }
-    else {
+    } else {
         wrap = 0;
     }
-    fprintf(filename, "%d ", g -> nb_rows);
-    fprintf(filename, "%d ", g -> nb_cols);
+    fprintf(filename, "%d ", g->nb_rows);
+    fprintf(filename, "%d ", g->nb_cols);
     fprintf(filename, "%d\n", wrap);
     for (uint i = 0; i < game_nb_rows(g); i++) {
         for (uint j = 0; j < game_nb_cols(g); j++) {
-            if (game_get_state(g, i, j) == S_LIGHTBULB){
+            if (game_get_state(g, i, j) == S_LIGHTBULB) {
                 fprintf(filename, "*");
             }
-            if (game_get_state(g, i, j) == S_BLACK0){
+            if (game_get_state(g, i, j) == S_BLACK0) {
                 fprintf(filename, "w");
             }
-            if (game_is_black(g, i, j) && game_get_state(g, i, j) != S_BLACK0){
+            if (game_is_black(g, i, j) && game_get_state(g, i, j) != S_BLACK0) {
                 fprintf(filename, "%d", game_get_black_number(g, i, j));
             }
-            if (game_is_blank(g, i, j)){
+            if (game_is_blank(g, i, j)) {
                 fprintf(filename, 'b');
             }
         }
